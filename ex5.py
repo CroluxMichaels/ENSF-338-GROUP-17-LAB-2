91d0d4464ff2317bcd7f0f628bc1a5429d041b2a
Crolu <chiruliacosta@gmail.com>
2025-02-11 15:29:06 -0700

    ex5 final touches

--- ex5.py
+++ ex5.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import scipy as sp
 import random as rand
 import timeit
-import math
 
 def linear_search(arr, key):
     found = -1
@@ -27,9 +26,6 @@ def binary_search(arr, key):
             low = mid + 1
             continue
     return -1 
-
-def logarithmic(x, a, b):
-    return a*math.log(x) + b
 
 list_sizes = [1000, 2000, 4000, 8000, 16000, 32000]
 avg_linear_times = []
@@ -55,23 +51,6 @@ for i in list_sizes:
 print(avg_linear_times)
 print(avg_binary_times)
 
-
-# Ask about it tomorrow in the lecture. This one is not making a lot of sense yet, because it takes a long time to run it!
-# Here is a shorter version of the same code...
-
-
-"""
-for i in list_sizes:
-    print(f"Computing for ordered vector of {i} elements")
-    vector = []
-    for j in range(0, i):
-        vector.append(j)
-    find = rand.randrange(0, i)
-    avg_linear_times.append(timeit.timeit(stmt=lambda: linear_search(vector, find), number=100)/100)
-    avg_binary_times.append(timeit.timeit(stmt=lambda: binary_search(vector, find), number=100)/100)
-print(avg_linear_times)
-print(avg_binary_times)
-"""
 plt.figure()
 slope, intercept = np.polyfit(list_sizes, avg_linear_times, 1)
 plt.scatter(list_sizes, avg_linear_times)
@@ -82,18 +61,6 @@ plt.ylabel("Average search time (s)")
 plt.show()
 
 plt.figure()
-
-"""
-bin_constants = sp.optimize.curve_fit(logarithmic, list_sizes, avg_binary_times)
-bin_fit_a = bin_constants[0][0]
-bin_fit_b = bin_constants[0][1]
-
-binary_fit = []
-for i in list_sizes:
-    binary_fit.append(logarithmic(i, bin_fit_a, bin_fit_b))
-plt.plot(list_sizes, binary_fit)
-"""
-
 plt.plot(list_sizes, avg_binary_times)
 
 plt.xlabel("Size of Vector")
